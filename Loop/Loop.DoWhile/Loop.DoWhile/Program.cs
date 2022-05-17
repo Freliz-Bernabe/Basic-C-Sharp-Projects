f5cfda4d6f4b385177efbf77c3806182d5506c8a
Freliz-Bernabe <96715569+Freliz-Bernabe@users.noreply.github.com>
2022-05-16 23:00:55 -0700

    Add files via upload

---
+++ Loop/Loop.DoWhile/Loop.DoWhile/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Loop.DoWhile
+{
+    class Program
+        {
+            static void Main()
+            {
+                Console.WriteLine("Guess a number between 1 to 10?");
+                int number = Convert.ToInt32(Console.ReadLine());
+                bool isGuessed = number == 6;
+
+                do
+                {
+                    switch (number)
+                    {
+                        case 5:
+                            Console.WriteLine("You guessed 5. Try again.");
+                            Console.WriteLine("Guess a number between 1 to 10?");
+                            number = Convert.ToInt32(Console.ReadLine());
+                            break;
+                        case 2:
+                            Console.WriteLine("You guessed 2. Try again.");
+                            Console.WriteLine("Guess a number between 1 to 10?");
+                            number = Convert.ToInt32(Console.ReadLine());
+                            break;
+                        case 8:
+                            Console.WriteLine("You guessed 8. Try again.");
+                            Console.WriteLine("Guess a number between 1 to 10?");
+                            number = Convert.ToInt32(Console.ReadLine());
+                            break;
+                        case 6:
+                            Console.WriteLine("You guessed the number 6. That is correct!");
+                            isGuessed = true;
+                            break;
+                        default:
+                            Console.WriteLine("You are wrong.");
+                            Console.WriteLine("Guess a number between 1 to 10?");
+                            number = Convert.ToInt32(Console.ReadLine());
+                            break;
+                    }
+                }
+                while (!isGuessed);
+
+                Console.Read();
+            }
+        }
+    }
---
+++ Loop/Loop.While/Loop.While/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Loop
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Guess a number between 1 to 10?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 6;
+
+            while (!isGuessed)
+            {
+                switch (number)
+                {
+                    case 5:
+                        Console.WriteLine("You guessed 5. Try again.");
+                        Console.WriteLine("Guess a number between 1 to 10?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 2:
+                        Console.WriteLine("You guessed 2. Try again.");
+                        Console.WriteLine("Guess a number between 1 to 10?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 8:
+                        Console.WriteLine("You guessed 8. Try again.");
+                        Console.WriteLine("Guess a number between 1 to 10?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 6:
+                        Console.WriteLine("You guessed the number 6. That is correct!");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are wrong.");
+                        Console.WriteLine("Guess a number between 1 to 10?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
