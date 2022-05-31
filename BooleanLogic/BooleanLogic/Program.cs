abc41e5a524f130dc4ee95af492775af71a26859
Freliz-Bernabe <96715569+Freliz-Bernabe@users.noreply.github.com>
2022-05-30 22:11:55 -0700

    Add files via upload

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            int yearsOld = Convert.ToInt32(Console.ReadLine());
+            bool yearsAge = (yearsOld > 15 && yearsOld >= 15);
+            Console.WriteLine(yearsAge);
+            Console.ReadLine();
+
+            Console.WriteLine("Have you ever had a DUI?");
+            string hasDui = (Console.ReadLine());
+            bool haveDui = false;
+            Console.WriteLine(haveDui);
+            Console.ReadLine();
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int howMany = Convert.ToInt32(Console.ReadLine());
+            bool noOfTickets = (howMany < 4 && howMany <= 4);
+            Console.WriteLine(noOfTickets);
+            Console.ReadLine();
+
+            Console.WriteLine("Qualified?");
+            bool isQualified = (yearsOld > 15 && haveDui && howMany < 4);
+            Console.WriteLine(isQualified);
+            Console.ReadLine();
+
+        }
+    }
+}
+
