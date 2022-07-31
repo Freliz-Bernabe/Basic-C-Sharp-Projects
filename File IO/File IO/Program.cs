8381f6e99d59299e27f675be27e9cb3c3d86d8d0
Freliz-Bernabe <96715569+Freliz-Bernabe@users.noreply.github.com>
2022-07-31 14:39:23 -0700

    Add files via upload

---
+++ File IO/File IO/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace File_IO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type a number.");
+            int number = Convert.ToInt32(Console.ReadLine());
+            string text = "Here's the number you typed.";
+
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Freliz\Documents\Logs\log.txt", true)) 
+            {
+                file.WriteLine(number);
+            }
+        }
+    }
+}
