790e13430cf65386faae2e7dfb70b0ee5373963e
Freliz-Bernabe <96715569+Freliz-Bernabe@users.noreply.github.com>
2022-08-08 13:07:31 -0700

    Add files via upload

---
+++ Struct/Struct/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    class Program
+    {
+        public struct Number 
+        {
+            public decimal Amount;
+        }
+        static void Main(string[] args)
+        {
+            Number price; 
+            price.Amount = 66.8m; 
+            Console.WriteLine(price.Amount);
+            Console.ReadLine();
+        }
+    }
+}
