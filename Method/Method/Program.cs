7b1cccd746cd50a56fc84529aa2c766ba9fda306
Freliz-Bernabe <96715569+Freliz-Bernabe@users.noreply.github.com>
2022-08-08 14:43:35 -0700

    Add files via upload

---
+++ Method/Method/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            methods math = new methods();
+            Console.WriteLine("Please enter a number to do some math operations with: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The number you entered plus 5 = " + math.mathOperation1(input)); 
+            Console.WriteLine("The number you entered minus 5 = " + math.mathOperation2(input)); 
+            Console.WriteLine("The number you entered times 5 = " + math.mathOperation3(input));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Method/Method/methods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    class methods
+    {
+        public int mathOperation1(int addingNumber)
+        {
+            int total = addingNumber + 5;
+            return total;
+        }
+        public int mathOperation2(int subtractionNumber)
+        {
+            int total = subtractionNumber - 5;
+            return total;
+        }
+        public int mathOperation3(int timesNumber)
+        {
+            int total = timesNumber * 5;
+            return total;
+        }
+    }
+}
