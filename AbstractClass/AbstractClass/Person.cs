1ae1df35d8b5e0abb3eac4d110f834d60a41114d
Freliz-Bernabe <96715569+Freliz-Bernabe@users.noreply.github.com>
2022-08-08 14:57:18 -0700

    Add files via upload

---
+++ AbstractClass/AbstractClass/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    public abstract class Person  
+    {
+        public string FirstName; 
+        public string LastName; 
+        public abstract void SayName();
+    }
+
+    public class Employees : Person
+    {
+        public override void SayName() 
+        {
+            Console.WriteLine(FirstName + " " + LastName); 
+        }
+    }
+}
---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employees name = new Employees() { FirstName = "Sample", LastName = "Student" }; 
+            name.SayName(); 
+            Console.ReadLine();
+        }
+    }
+}
