2b361dc8fdc5c871eaad5f11f1631d34e6e21dc0
Freliz-Bernabe <96715569+Freliz-Bernabe@users.noreply.github.com>
2022-08-08 12:52:06 -0700

    Add files via upload

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employee = new Employee<string>(); 
+            Employee<int> employeeId = new Employee<int>();
+            employee.Things = new List<string>();
+            employeeId.Things = new List<int>(); 
+
+            employee.Things.Add("Fredelyn");
+            employee.Things.Add("Frederick");
+            employee.Things.Add("Ferdinand");
+            employeeId.Things.Add(1); 
+            employeeId.Things.Add(2);
+            employeeId.Things.Add(3);
+
+            foreach (string name in employee.Things) 
+            {
+                Console.WriteLine(name);
+            }
+            foreach (int Id in employeeId.Things)
+            {
+                Console.WriteLine(Id);
+            }
+            Console.ReadLine();
+        }
+    }
+}
