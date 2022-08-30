ed46fc411053ba2a7b80fa7390c68fe209567bb6
Freliz-Bernabe <96715569+Freliz-Bernabe@users.noreply.github.com>
2022-08-29 21:58:54 -0700

    Add files via upload

---
+++ EF6Console_1/DAL/SchoolContext.cs
@@ -0,0 +1,21 @@
+﻿using EF6Console.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EF6Console.DAL
+{
+    public class SchoolContext : DbContext
+    {
+            public SchoolContext() : base()
+            {
+
+            }
+
+            public DbSet<Student> Students { get; set; }
+            public DbSet<Grade> Grades { get; set; }
+        
+    }
+}
---
+++ EF6Console_1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EF6Console.MvcApplication" Language="C#" %>
