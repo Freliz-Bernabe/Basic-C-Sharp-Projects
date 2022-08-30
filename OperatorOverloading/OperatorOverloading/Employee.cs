27b903506a3ea897186f5e2a4395e8fbaf00f4f8
Freliz-Bernabe <96715569+Freliz-Bernabe@users.noreply.github.com>
2022-08-29 22:09:59 -0700

    Add files via upload

---
+++ OperatorOverloading/OperatorOverloading/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloading
+{
+    class Employee
+    {
+        public int Id { get; set; }
+
+        public static bool operator ==(Employee person1, Employee person2) 
+        {
+            if (person1==person2)
+            {
+                return true; 
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator !=(Employee person1, Employee person2)
+        {
+            if (person1!=person2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
