6f3c500b19c1643fa4628c846dd5aa1ed7f70237
Freliz-Bernabe <96715569+Freliz-Bernabe@users.noreply.github.com>
2022-09-05 14:08:38 -0700

    Add files via upload

---
+++ ClassesAndMethods3/ClassesAndMethods3/ClassesAndMethods3/MathOperation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ClassesAndMethods3
+{
+    public class MathOperation
+    {
+        public static void mathOp(int numberOne, int numberTwo)
+        {
+            Console.WriteLine(numberOne * 2 + 100);
+            Console.WriteLine(numberTwo);
+        }
+    }
+}
