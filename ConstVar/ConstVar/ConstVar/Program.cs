041be10ba6d958464c7352005bea15dcdb04a9bc
Freliz-Bernabe <96715569+Freliz-Bernabe@users.noreply.github.com>
2022-09-05 14:20:43 -0700

    Add files via upload

---
+++ ConstVar/ConstVar/ConstVar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string hiFrance = "Bonjour"; 
+            Console.WriteLine(hiFrance);
+
+            var user1 = new User("Miki", 800); 
+            Console.WriteLine(user1.Name);
+            Console.WriteLine(user1.Phone);
+            Console.Read();
+        }
+    }
+}
---
+++ ConstVar/ConstVar/ConstVar/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVar
+{
+    public class User
+    {
+        public string Name;
+        public int Phone;
+
+        public User() //mosh tutorial advised including default constructor so that I do not have to add default parameters
+        {
+        }
+        public User(string userName)
+        {
+            this.Name = userName;
+        }
+        public User(string userName, int userPhone)
+            : this(userName) //passes name constructor in so it calls the name constructor first
+        {
+            this.Phone = userPhone;
+        }
+    }
+}
