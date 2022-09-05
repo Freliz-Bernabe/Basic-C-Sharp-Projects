c6554346b06b23476f29faa54ba27a3354179397
Freliz-Bernabe <96715569+Freliz-Bernabe@users.noreply.github.com>
2022-09-05 13:54:45 -0700

    Add files via upload

---
+++ ClassesAndMethods2/ClassesAndMethods2/ClassesAndMethods2/Integer.cs
@@ -0,0 +1,14 @@
+﻿namespace ClassesAndMethods2
+{
+        public class Integer
+        {
+            public static int Sum(int num1, int num2)
+            {
+                int total;
+                total = num1 + num2;
+                return total;
+
+            }
+        }
+
+}
