782810f92b0eaa9fc6c7a6f342de3e452b6ea886
Freliz-Bernabe <96715569+Freliz-Bernabe@users.noreply.github.com>
2022-09-04 21:14:04 -0700

    Add files via upload

---
+++ EF Code-First/Models/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EF_Code_First.Models
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Bill" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
---
+++ EF Code-First/Models/SchoolContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EF_Code_First.Models
+{
+    public class SchoolContext
+    {
+        public SchoolContext() : base()
+            {
+
+            }
+
+            public DbSet<Student> Students { get; set; }
+            public DbSet<Grade> Grades { get; set; }
+
+        internal void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
