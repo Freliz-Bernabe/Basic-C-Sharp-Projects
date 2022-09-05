c790fa8a5515206cb4169de7e75c99a0fb6bcf64
Freliz-Bernabe <96715569+Freliz-Bernabe@users.noreply.github.com>
2022-09-05 14:29:56 -0700

    Add files via upload

---
+++ ClassMethod/ClassMethod/ClassMethod/MathOp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace ClassMethod
+{
+    public class MathOp
+    {
+        public void MathMethodFirst(int num1, int num2)
+        {
+            int result = num1 / 2;
+            int result2 = num2 / 2;
+            Console.WriteLine("Your number divided by 2 = " + result2);
+
+        }
+
+        public void MathMethodSecond(int num3, int num4)
+        {
+            int result5 = num3 * num4;
+            Console.WriteLine(num3 + " x " + num4 + " = " + result5);
+        }
+
+        public static int MathMethodSecond(int num1, out int num2)
+        {
+            num2 = num1 + 5;
+            return num2;
+
+        }
+    }
+}
---
+++ ClassMethod/ClassMethod/ClassMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number.");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int num1 = 10;
+            int dog = 100;
+            int cat = 7;
+
+            MathOp FirstOp = new MathOp();
+            FirstOp.MathMethodFirst(num1, num2);
+            FirstOp.MathMethodSecond(dog, cat);
+
+            Console.ReadLine();
+        }
+    }
+}
